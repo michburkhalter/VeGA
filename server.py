94b363566286b13d7bbd9caa4d0ea5e21c639d66
Michael Burkhalter <michburkhalter@gmail.com>
2024-11-08 21:12:35 +0100

    asdf

--- server.py
+++ server.py
@@ -86,4 +86,4 @@ def logout():
 
 # asdf
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=env.get("PORT", 3000), debug=False)
+    app.run(host="0.0.0.0", port=env.get("PORT", 3000), debug=True)
